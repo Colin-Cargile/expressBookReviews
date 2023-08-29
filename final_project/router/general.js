fca8e0467e0d6724291e2fae3e1a6b4b9565f745
Colin-Cargile <141424944+Colin-Cargile@users.noreply.github.com>
2023-08-29 12:59:33 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,8 +9,6 @@ public_users.post("/register", (req,res) => {
   //Write your code here
   const username = req.body.username;
   const password = req.body.password;
-  console.log(req.body)
-  console.log(username)
   if (username && password) {
     if (!doesExist(username)) { 
       users.push({"username":username,"password":password});
@@ -31,7 +29,36 @@ const doesExist = (username)=>{
       return false;
     }
   }
+  public_users.post("/login", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+   if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+  
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }});
 
+  const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
